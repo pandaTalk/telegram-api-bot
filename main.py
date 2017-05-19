f77c35abdf9cf9792d977c56ac49727b8fb532d6
Johnny Chan <johnny.chan@neoderm.com.hk>
2017-05-19 15:17:36 +0800

    update main, update readme

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import json
 import requests
-
+from bs4 import BeautifulSoup as bs
 
 def request_USDT_BTC():
     r = requests.get('http://poloniex.com/public?command=returnTicker')
@@ -16,6 +16,20 @@ def request_hsi():
     value = parsedjson["indexInfos"][0]["index"]
     return str(value)
 
+def request_stock(quoteSymbol):
+    r = requests.get(
+        "http://www.aastocks.com/tc/stocks/quote/detail-quote.aspx?symbol=" + quoteSymbol,
+        timeout=10)
+    responseCookies = r.cookies
+    responseCookies["AALTP"] = '1'
+    r = requests.get(
+        "http://www.aastocks.com/tc/stocks/quote/detail-quote.aspx?symbol=" + quoteSymbol,
+        cookies=responseCookies)
+
+    html_body = bs(r.text, 'html.parser')
+    value = html_body.select_one("#labelLast > b > span").contents[1]
+    return str(value)
+
 def sendMessage(chat_id, message):
     payload = {"chat_id": chat_id, "text": message}
     requests.post(
@@ -23,7 +37,6 @@ def sendMessage(chat_id, message):
         data=payload)
 
 def main(event, context):
-    print("Received event: " + json.dumps(event, indent=2))
     input_json = json.loads(event["body"])
     if "message" in input_json:
         input_json = input_json["message"]
@@ -33,6 +46,8 @@ def main(event, context):
                 is_command = input_json["entities"][0]["type"] == "bot_command"
                 if is_command:
                     command = input_json["text"]
+                    offset = input_json["entities"][0]["offset"]
+                    length = input_json["entities"][0]["length"]
                     message = ""
                     if "/getbtc" in command:
                         message = request_USDT_BTC()
@@ -41,9 +56,16 @@ def main(event, context):
                     elif "/getall" in command:
                         message = "BTC: " + request_USDT_BTC()
                         message += "\nHSI: " + request_hsi()
+                    elif "/getstock" in command:
+                        qouteNumber = command[length:]
+                        if qouteNumber:
+                            message = "Stock " + qouteNumber + ": " + request_stock(qouteNumber)
+                        else:
+                            message = "Require Stock Symbol"
+
 
                     if message != "":
                         sendMessage(chat_id, message)
 
-    res = {"statusCode": 200, "headers": {}, "body": event["body"]}
+    res = {"statusCode": 200, "headers": {}}
     return res
--- readme.md
+++ readme.md
@@ -9,9 +9,11 @@ Get the HSI and BTC (and more coming) in telegram bot
 1. Trigger by bot command
     
     main.py
+        1. Quote BTC Price
+        2. Quote HK Stock Price
+        3. Quote HSI 
 
 2. Auto trigger by AWS CloudWatch Schedule
 
     auto_trigger.py
-
 
